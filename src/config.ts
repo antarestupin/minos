dc862804cdab40d06b5d006827e992d5e932c5c3
antarestupin <antares.tupin@gmail.com>
2018-11-25 18:37:38 +0100

    Set global config path

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const globalConfigPath = `${process.env.HOME}/workspace/github/minos/example/.minos.json`;
+export const globalConfigPath = `${process.env.HOME}/.minos.json`;
 
 export const defaultUserProjectConfig = {
   repositoryType: 'git',
